96520a07d2643524f9f818e77ff5e30b4a4bd645
vishnuadepu82 <vishnu.adepu82@gmail.com>
2021-02-16 18:53:46 +0530

    1 file modified, 22 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ArthService } from './arth.service';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { StudentHomeComponent } from './student-home/student-home.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { EnrollmentsComponent } from './enrollments/enrollments.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MsgBoxComponent } from './msg-box/msg-box.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent, AdminDashboardComponent, AdminHomeComponent, StudentDashboardComponent, StudentHomeComponent, StudentFormComponent, AddCourseComponent, CourseListComponent, EnrollmentsComponent, MenuBarComponent, MsgBoxComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ArthService]
 })
---
+++ src/app/msg-box/msg-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-msg-box',
+  templateUrl: './msg-box.component.html',
+  styleUrls: ['./msg-box.component.css']
+})
+export class MsgBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
