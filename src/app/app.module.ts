1580001f21be01ee83676ea3299df67107c1979b
vishnuadepu82 <vishnu.adepu82@gmail.com>
2021-02-18 17:19:49 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 import { ArthService } from './arth.service';
 import { LoginComponent } from './login/login.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
-import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
-import { StudentHomeComponent } from './student-home/student-home.component';
 import { StudentFormComponent } from './student-form/student-form.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { CourseListComponent } from './course-list/course-list.component';
@@ -26,7 +22,7 @@ import { StudenthomeComponent } from './studenthome/studenthome.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, LoginComponent, AdminDashboardComponent, AdminHomeComponent, StudentDashboardComponent, StudentHomeComponent, StudentFormComponent, AddCourseComponent, CourseListComponent, EnrollmentsComponent, MenuBarComponent, MsgBoxComponent, AdminhomeComponent, AdmindashboardComponent, StudentdashboardComponent, StudenthomeComponent ],
+  declarations: [ AppComponent, LoginComponent,     StudentFormComponent, AddCourseComponent, CourseListComponent, EnrollmentsComponent, MenuBarComponent, MsgBoxComponent, AdminhomeComponent, AdmindashboardComponent, StudentdashboardComponent, StudenthomeComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ArthService, StudentService, CourseService, UserService]
 })
