a571d4242133ee18f98c325f3cfa0e3346e573e9
vishnuadepu82 <vishnu.adepu82@gmail.com>
2021-02-18 17:34:50 +0530

    2 files modified

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserService } from "../user.service";
 
 @Component({
-  selector: 'app-menu-bar',
-  templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  selector: "app-menu-bar",
+  templateUrl: "./menu-bar.component.html",
+  styleUrls: ["./menu-bar.component.css"]
 })
 export class MenuBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  links: string[][];
+  isLoggedin: boolean;
+  constructor(private userservice: UserService, private router: Router) {
+    this.links = [];
+    this.isLoggedin = false;
   }
 
+  ngOnInit() {
+    if (this.userservice.isLoggedin()) {
+      this.isLoggedin = true;
+      let role = this.userservice.currentUser().role;
+      if (role == "ADMIN") {
+        this.links = [
+          ["/admin", "Home"],
+          ["/admin/courses", "Courses"],
+          ["/admin/addcourse", "Add Course"]
+        ];
+      } else {
+        this.links = [
+          ["/student", "Home"],
+          ["/student/courses", "Courses"],
+          ["/student/enrolls", "enrollments"],
+          ["/admin/edit", "Profile"]
+        ];
+      }
+    }
+    else {
+        this.links = [
+          ["/", "Home"],
+          ["/signup", "Sign Up"]
+        ];
+      }
+  }
 }
